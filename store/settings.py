c6e48a18a9a1aebab40d999fde2bc99167bad207
ebarbara <estevan.barbara@gmail.com>
2022-03-14 13:53:39 -0300

    Heroku updates

--- store/settings.py
+++ store/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 from decouple import config, Csv
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +144,6 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
 }
+
+# Heroku config
+django_on_heroku.settings(locals())
